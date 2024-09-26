859a8c0a31c5121808295ef8769182fc0f8c26c0
Victor GC <vgarcanuk@gmail.com>
2024-09-26 19:51:15 +0100

    UPDATE: orders app created and models defined

---
+++ red-bazaar/orders/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ShoppingList(models.Model):
+    buyer = models.ForeignKey('users.CustomUser', on_delete=models.PROTECT)
+    product_id = models.ManyToManyField('products.Product')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class OrderList(models.Model):
+    buyer = models.ForeignKey(
+        'users.CustomUser', on_delete=models.PROTECT, related_name='order_lists_as_buyer')
+    seller = models.ForeignKey(
+        'users.CustomUser', on_delete=models.PROTECT, related_name='order_lists_as_seller')
+    shoppinglist_id = models.OneToOneField(
+        ShoppingList, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    ordered_on = models.DateTimeField(auto_now_add=True)
