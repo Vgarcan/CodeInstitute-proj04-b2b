c341a2c884b350abe9a339934d67f6f52006e5d6
Victor GC <vgarcanuk@gmail.com>
2024-11-28 01:15:28 +0000

    UPDATE: ToolTips included.  navbar icon colour changed.

--- static/js/main.js
+++ static/js/main.js
@@ -26,3 +26,8 @@ document.addEventListener('DOMContentLoaded',
             navbarCollapse.classList.toggle('collapse');
         });
     });
+
+    // BOOTSTRAP - Enable ToolTips
+    // https://getbootstrap.com/docs/5.3/components/tooltips/#enable-tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
